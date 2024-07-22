6305d3a5177234983990d59d5ead9ff44e696398
oleksandr <boltbright525@gmail.com>
2024-07-22 17:41:54 -0300

    feature: delay page paths build

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import { getOneProduct } from '../../data/products/getOne';
 import ProductPage from '../../containers/Product';
 
 import { ProductProtocol } from '../../domain/products/product-protocol';
+import { delay } from '../../../prodUtils/delayPageBuild';
 
 export type ProductProps = {
   product: ProductProtocol;
@@ -26,6 +27,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params;
+  await delay(200);
 
   const product = await getOneProduct(String(id));
 
