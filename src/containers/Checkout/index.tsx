bb7c40728361aafec2a9c13d69e806988cabf623
oleksandr <boltbright525@gmail.com>
2024-08-13 14:34:20 -0300

    fix: using redirect in hook closedPages

--- src/containers/Checkout/index.tsx
+++ src/containers/Checkout/index.tsx
@@ -1,16 +1,9 @@
-import { useRouter } from 'next/router';
 import { useClosedPage } from '../../hooks/closedPage';
 
 import { Container } from './styled';
 
 export default function Checkout() {
-  const router = useRouter();
-
   const { isClosed, LSLoaded } = useClosedPage();
-
-  if (LSLoaded && isClosed) {
-    router.push(`/profile?redirect=${encodeURIComponent('/checkout')}`);
-  }
 
   return LSLoaded && !isClosed && <Container>CHECKOUT</Container>;
 }
--- src/hooks/closedPage.ts
+++ src/hooks/closedPage.ts
@@ -1,8 +1,15 @@
+import { useRouter } from 'next/router';
 import { useUserContext } from '../contexts/user';
 
-export const useClosedPage = () => {
+export const useClosedPage = (redirect: boolean = true) => {
+  const router = useRouter();
+
   const { user, LSLoaded } = useUserContext();
   const isClosed = !user.isLoggedIn;
+
+  if (redirect && LSLoaded && isClosed) {
+    router.push(`/profile?redirect=${encodeURIComponent('/checkout')}`);
+  }
 
   return { isClosed, LSLoaded };
 };
