02eab1e5442cd71bfb9809ae8189f15942c1d004
oleksandr <boltbright525@gmail.com>
2024-08-13 22:52:18 -0300

    feature: max width of 676 cards page

--- src/containers/Cards/styled.ts
+++ src/containers/Cards/styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.main`
   margin-inline: auto;
   margin-bottom: 2rem;
+  max-width: 676px;
 
   section ~ section {
     margin-top: 2rem;
