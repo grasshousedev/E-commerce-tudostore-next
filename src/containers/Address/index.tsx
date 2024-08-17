4fdf20a9b11d7595159ef228585b9c1aa40f7e59
oleksandr <boltbright525@gmail.com>
2024-08-16 23:38:12 -0300

    feature: Form component to /profile/address/

--- src/containers/Address/index.tsx
+++ src/containers/Address/index.tsx
@@ -1,5 +1,78 @@
+import Form from '../../components/Form';
+
+import { CardSection } from '../../styles/card';
 import { Container } from './styled';
 
+const NewAddress = () => {
+  return (
+    <CardSection className="m-padding">
+      <header>
+        <h1 className="title-all-uppercase-spaced">Adicionar novo cartão</h1>
+      </header>
+      <Form
+        fields={[
+          {
+            input: {
+              type: 'text',
+              name: 'cardholder-name',
+              id: 'cardholder-name',
+              placeholder: 'Rebeca de Assis Machado',
+              value: '',
+              onChange: () => undefined,
+            },
+            label: {
+              content: 'Nome no cartão',
+              htmlFor: 'cardholder-name',
+            },
+          },
+          {
+            input: {
+              className: 'grey-placeholder pdg-l',
+              type: 'text',
+              name: 'card-number',
+              id: 'card-number',
+              placeholder: '💳  5126-5987-2214-7621',
+              value: '',
+              onChange: () => undefined,
+            },
+            label: {
+              content: 'Numero do cartão',
+              htmlFor: 'card-number',
+            },
+          },
+          {
+            input: {
+              className: 'grey-placeholder pdg-l',
+              type: 'text',
+              name: 'new-name-to-it',
+              id: 'new-name-to-it',
+              placeholder: '',
+              value: '',
+              onChange: () => undefined,
+            },
+            label: {
+              content: 'Numero do cartão',
+              htmlFor: 'new-name-to-it',
+            },
+          },
+        ]}
+        onSubmitAction={(e) => e.preventDefault()}
+        submitBtnContent={'Adicionar Endereço'}
+        footer={{
+          backButton: {
+            onClick: () => history.back(),
+            content: 'Voltar',
+          },
+        }}
+      />
+    </CardSection>
+  );
+};
+
 export default function Address() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <NewAddress />
+    </Container>
+  );
 }
--- src/containers/Address/styled.ts
+++ src/containers/Address/styled.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > section {
+    max-width: 676px;
+    width: 100%;
+  }
+`;
