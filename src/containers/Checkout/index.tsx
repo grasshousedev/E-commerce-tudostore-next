6b743deb010b26a1af338d296028f1fe9851edea
oleksandr <boltbright525@gmail.com>
2024-08-20 19:14:05 -0300

    feature: success checkout component

--- src/containers/Checkout/index.tsx
+++ src/containers/Checkout/index.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { useEffect, useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import { IoIosArrowBack } from 'react-icons/io';
+import { IoHome } from 'react-icons/io5';
 
 import { useUserContext } from '../../contexts/user';
 import { useBagContext } from '../../contexts/bag';
@@ -16,18 +18,46 @@ import Button from '../../components/Button';
 import BagItems from '../../components/BagItems';
 
 import { Card } from '../../styles/card';
-import { Container, ContainerResume, ContainerOrderInfos, ContainerOrderTotal } from './styled';
+import {
+  Container,
+  ContainerResume,
+  ContainerOrderInfos,
+  ContainerOrderTotal,
+  ContainerCheckoutFinal,
+} from './styled';
 
 export type requirementsToBuyProtocol = {
   haveAddress: boolean | null;
   haveCard: boolean | null;
 };
 
+export type CheckoutSuccessProps = {
+  router: AppRouterInstance;
+};
+
+const CheckoutSuccess = ({ router }: CheckoutSuccessProps) => {
+  return (
+    <ContainerCheckoutFinal>
+      <div className="success-animation">
+        <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+          <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+          <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+        </svg>
+      </div>
+      <h1>Compra feita com sucesso!</h1>
+      <Button onClick={() => router.push('/')}>
+        <IoHome />
+        <span>Voltar para a loja</span>
+      </Button>
+    </ContainerCheckoutFinal>
+  );
+};
+
 export default function Checkout() {
   const router = useRouter();
 
   const { isClosed, LSLoaded } = useClosedPage('/checkout');
-  const { bagTotal, bagData, LSLoaded: BagLSLoaded, bagItems } = useBagContext();
+  const { bagTotal, bagData, LSLoaded: BagLSLoaded, bagItems, setBagItems } = useBagContext();
   const { cards, address } = useUserContext();
 
   const [total, setTotal] = useState(0);
@@ -35,7 +65,7 @@ export default function Checkout() {
     haveAddress: null,
     haveCard: null,
   });
-  const [useCanBuy, setUserCanBuy] = useState<null | boolean>(null);
+  const [userCanBuy, setUserCanBuy] = useState<null | boolean>(null);
 
   const shippingCost = useMemo(() => parseFloat((Math.random() * 300).toFixed(2)), []);
   const taxService = useMemo(() => parseFloat((Math.random() * 30).toFixed(2)), []);
@@ -48,13 +78,13 @@ export default function Checkout() {
   }, [bagTotal, bagData]);
 
   useEffect(() => {
-    if (BagLSLoaded && bagItems.length === 0) {
+    if (!userCanBuy && BagLSLoaded && bagItems.length === 0) {
       router.push('/');
       toast.dismiss();
       toast('Você não tem items no seu carrinho para fazer checkout');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [BagLSLoaded, bagItems]);
+  }, [userCanBuy, BagLSLoaded, bagItems]);
 
   const handleBuy = () => {
     const haveDefaultCard = cards.find((card) => card.isDefault);
@@ -100,57 +130,58 @@ export default function Checkout() {
       haveAddress: true,
       haveCard: true,
     });
+    setBagItems([]);
   };
 
   return (
-    bagItems.length > 0 &&
-    bagData.length > 0 &&
-    LSLoaded &&
-    !isClosed && (
-      <Container>
-        <section className="left-container">
-          <AddressCard
-            className={`m-border-radius ${requirementsToBuy.haveAddress === false ? 'error' : ''}`}
-            Title={'h1'}
-          />
-          <CardCard
-            className={`m-border-radius ${requirementsToBuy.haveCard === false ? 'error' : ''}`}
-            Title={'h1'}
-          />
-          <Card className="m-border-radius">
-            <header>
-              <h1 className="title-all-uppercase-spaced">Reveja sua bolsa</h1>
-            </header>
-            <BagItems className="m-top-1 border-divisor" />
-          </Card>
-        </section>
-        <section className="right-content">
-          <ContainerResume>
-            <header>
-              <h1>Resumo da Ordem</h1>
-            </header>
-            <ContainerOrderInfos>
-              <div>
-                <span>Items:</span> <span>{convertToBRL(bagTotal)}</span>
-              </div>
-              <div>
-                <span>Envio:</span> <span>{convertToBRL(shippingCost)}</span>
-              </div>
-              <div>
-                <span>Taxa Serviço:</span> <span>{convertToBRL(taxService)}</span>
-              </div>
-            </ContainerOrderInfos>
-            <ContainerOrderTotal>
-              <span>Total:</span> <span>{convertToBRL(total)}</span>
-            </ContainerOrderTotal>
-            <Button onClick={bagData.length > 0 ? handleBuy : () => undefined}>Finalizar Ordem</Button>
-          </ContainerResume>
-          <Button className="outline" onClick={() => history.back()}>
-            <IoIosArrowBack />
-            <span>Voltar</span>
-          </Button>
-        </section>
-      </Container>
-    )
+    <>
+      {!userCanBuy && bagItems.length > 0 && bagData.length > 0 && LSLoaded && !isClosed && (
+        <Container>
+          <section className="left-container">
+            <AddressCard
+              className={`m-border-radius ${requirementsToBuy.haveAddress === false ? 'error' : ''}`}
+              Title={'h1'}
+            />
+            <CardCard
+              className={`m-border-radius ${requirementsToBuy.haveCard === false ? 'error' : ''}`}
+              Title={'h1'}
+            />
+            <Card className="m-border-radius">
+              <header>
+                <h1 className="title-all-uppercase-spaced">Reveja sua bolsa</h1>
+              </header>
+              <BagItems className="m-top-1 border-divisor" />
+            </Card>
+          </section>
+          <section className="right-content">
+            <ContainerResume>
+              <header>
+                <h1>Resumo da Ordem</h1>
+              </header>
+              <ContainerOrderInfos>
+                <div>
+                  <span>Items:</span> <span>{convertToBRL(bagTotal)}</span>
+                </div>
+                <div>
+                  <span>Envio:</span> <span>{convertToBRL(shippingCost)}</span>
+                </div>
+                <div>
+                  <span>Taxa Serviço:</span> <span>{convertToBRL(taxService)}</span>
+                </div>
+              </ContainerOrderInfos>
+              <ContainerOrderTotal>
+                <span>Total:</span> <span>{convertToBRL(total)}</span>
+              </ContainerOrderTotal>
+              <Button onClick={bagData.length > 0 ? handleBuy : () => undefined}>Finalizar Ordem</Button>
+            </ContainerResume>
+            <Button className="outline" onClick={() => history.back()}>
+              <IoIosArrowBack />
+              <span>Voltar</span>
+            </Button>
+          </section>
+        </Container>
+      )}
+      {userCanBuy && <CheckoutSuccess router={router} />}
+    </>
   );
 }
